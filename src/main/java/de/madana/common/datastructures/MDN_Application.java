4946cd4f9941c89ee15d2c3e15dcbddc480523c2
J.-Fabian Wenisch <wenisch@madana.io>
2018-11-20 17:38:24 +0100

    DEV-903 #time 5m #comment Added Application POM

---
+++ src/main/java/de/madana/common/datastructures/MDN_Application.java
@@ -0,0 +1,14 @@
+package de.madana.common.datastructures;
+
+public class MDN_Application 
+{
+private String pem;
+
+public String getPem() {
+	return pem;
+}
+
+public void setPem(String pem) {
+	this.pem = pem;
+}
+}
