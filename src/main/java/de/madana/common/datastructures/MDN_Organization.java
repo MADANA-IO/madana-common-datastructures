a36bcd1bdb5bcd92f9a2bc10ae89bb1c072bd7d7
J.-Fabian Wenisch <wenisch@madana.io>
2018-11-22 13:22:37 +0100

    DEV-903 #time 2m #comment Added Getter / Setter for pojo

--- src/main/java/de/madana/common/datastructures/MDN_Organization.java
+++ src/main/java/de/madana/common/datastructures/MDN_Organization.java
@@ -4,4 +4,16 @@ public class MDN_Organization
 {
 String name;
 String description;
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getDescription() {
+	return description;
+}
+public void setDescription(String description) {
+	this.description = description;
+}
 }
