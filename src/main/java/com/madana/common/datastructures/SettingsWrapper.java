3521cedbe109234b384fca8877022f4bd8e8ca16
J.-Fabian Wenisch <wenisch@madana.io>
2021-02-09 13:05:30 +0100

    fix: Settingswrapper pojo

---
+++ src/main/java/com/madana/common/datastructures/SettingsWrapper.java
@@ -0,0 +1,35 @@
+package com.madana.common.datastructures;
+
+public class SettingsWrapper 
+{
+	public String value;
+	public String name;
+	public String Description;
+	public String type;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return Description;
+	}
+	public void setDescription(String description) {
+		Description = description;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+
+}
