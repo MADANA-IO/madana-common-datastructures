473c204ddcc3c4b19ad49af6433b773f6207101b
J.-Fabian Wenisch <wenisch@madana.io>
2021-02-09 11:33:48 +0100

    fix: added node related pojos

---
+++ src/main/java/com/madana/common/datastructures/node/SGXInfo.java
@@ -0,0 +1,19 @@
+package com.madana.common.datastructures.node;
+
+public class SGXInfo {
+	String status;
+	String version;
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getVersion() {
+		return version;
+	}
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+}
